01490ef191c015e10bed2b55f8857ae1c3331754
dorkem <20233144@gs.cwnu.ac.kr>
2025-01-17 11:25:34 +0900

    Create the Book class.

---
+++ week2/q2/Book.java
@@ -0,0 +1,28 @@
+package week2.q2;
+
+public class Book {
+    private String bookName;
+    private String author;
+
+    public Book(String bookName, String author){
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
---
+++ week2/q2/Library.java
@@ -0,0 +1,11 @@
+package week2.q2;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+
+public class Library {
+    public static void main(String[] args) {
+        ArrayList<Book> A = new ArrayList<Book>();
+
+    }
+}
