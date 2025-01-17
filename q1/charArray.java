b976391a6a1e263c654ef868f7fcadd2448ff179
dorkem <20233144@gs.cwnu.ac.kr>
2025-01-17 11:12:25 +0900

    Create a 26-element char array and store each character from 'A' to 'Z' using ASCII values.

---
+++ q1/charArray.java
@@ -0,0 +1,11 @@
+package q1;
+
+public class charArray {
+    public static void main(String[] args) {
+        char[] alphabet = new char[26];
+        for(int i = 0; i<=25; i++) alphabet[i] = (char) (i + 65);
+        for(int i=0;i<alphabet.length;i++){
+            System.out.print(alphabet[i] + " ");
+        }
+    }
+}
