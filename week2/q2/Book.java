fc6b7ea6ea6257180148e31d477fe63448f1580a
dorkem <20233144@gs.cwnu.ac.kr>
2025-01-17 13:52:49 +0900

    Implement Library class with shallow and deep copy functionality.

--- week2/q2/Book.java
+++ week2/q2/Book.java
@@ -1,5 +1,7 @@
 package week2.q2;
 
+import java.util.ArrayList;
+
 public class Book {
     private String bookName;
     private String author;
@@ -9,6 +11,32 @@ public class Book {
         this.author = author;
     }
 
+    public static ArrayList<Book> initBooks(){
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("태백산맥", "조정래"));
+        books.add(new Book("이기적 유전자", "리처드 도킨스"));
+        books.add(new Book("자전거 도둑", "박완서"));
+        books.add(new Book("토지", "박경리"));
+        books.add(new Book("대변동", "제레드 다이아몬드"));
+        return books;
+    }
+
+    public static ArrayList<Book> deepCopy(ArrayList<Book> base){
+        ArrayList<Book> books = new ArrayList<>();
+        for (Book book : base) {
+            books.add(new Book(book.bookName, book.author));
+        }
+        return books;
+    }
+
+    public static void printBooks(ArrayList<Book> books, String library) {
+        System.out.println("도서관" + library + "의 책 목록:");
+        for (Book book : books) {
+            System.out.println("제목: " + book.getBookName() + ", 저자: " + book.getAuthor());
+        }
+        System.out.println();
+    }
+
     public String getBookName() {
         return bookName;
     }
@@ -16,13 +44,4 @@ public class Book {
     public String getAuthor() {
         return author;
     }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
 }
--- week2/q2/Library.java
+++ week2/q2/Library.java
@@ -1,11 +1,17 @@
 package week2.q2;
-
-import java.awt.print.Book;
 import java.util.ArrayList;
 
 public class Library {
     public static void main(String[] args) {
-        ArrayList<Book> A = new ArrayList<Book>();
+        ArrayList<Book> A = Book.initBooks();
+        ArrayList<Book> A1 = A;
+        ArrayList<Book> B = Book.deepCopy(A);
 
+        A.set(2, new Book("그 많던 싱아는 누가 다 먹었을까?","박완서"));
+        B.add(new Book("사피엔스","유발 하라리"));
+
+        Book.printBooks(A, "A");
+        Book.printBooks(A1, "A-1");
+        Book.printBooks(B,"B");
     }
 }
