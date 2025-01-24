b5f9cc9488b68f8e4545339d074478aa02cb4875
dorkem <20233144@gs.cwnu.ac.kr>
2025-01-24 15:25:52 +0900

    Complete basic framework setup.

---
+++ week3/Problem2/gradeSystem/GradeEvaluation.java
@@ -0,0 +1,5 @@
+package week3.Problem2.gradeSystem;
+
+public interface GradeEvaluation {
+    String getGrade(int point);
+}
---
+++ week3/Problem2/gradeSystem/GradingSystemMain.java
@@ -0,0 +1,7 @@
+package week3.Problem2.gradeSystem;
+
+public class GradingSystemMain {
+    public static void main(String[] args) {
+
+    }
+}
